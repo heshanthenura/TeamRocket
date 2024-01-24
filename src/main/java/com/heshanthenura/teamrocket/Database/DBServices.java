1225a74333ad9138d9596df528a8c10239f9a14f
Heshan Thenura Kariyawasam <heshanthenura@protonmail.com>
2024-01-25 00:15:53 +0530

    added update, delete and search features

--- src/main/java/com/heshanthenura/teamrocket/Database/DBServices.java
+++ src/main/java/com/heshanthenura/teamrocket/Database/DBServices.java
@@ -33,6 +33,34 @@ public class DBServices {
         }
     }
 
+    public void updatePerson(Person oldPerson, Person newPerson) {
+        try (Connection connection = DBConnection.getConnection()) {
+            String updatePersonSQL = "UPDATE person SET name=?, netWorth=?, age=?, countryOrTerritory=?, source=?, industry=? WHERE name=? AND netWorth=? AND age=? AND countryOrTerritory=? AND source=? AND industry=?";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(updatePersonSQL)) {
+                preparedStatement.setString(1, newPerson.getName());
+                preparedStatement.setString(2, newPerson.getNetWorth());
+                preparedStatement.setInt(3, newPerson.getAge());
+                preparedStatement.setString(4, newPerson.getCountryOrTerritory());
+                preparedStatement.setString(5, newPerson.getSource());
+                preparedStatement.setString(6, newPerson.getIndustry());
+
+                // Use the old person's values for WHERE clause
+                preparedStatement.setString(7, oldPerson.getName());
+                preparedStatement.setString(8, oldPerson.getNetWorth());
+                preparedStatement.setInt(9, oldPerson.getAge());
+                preparedStatement.setString(10, oldPerson.getCountryOrTerritory());
+                preparedStatement.setString(11, oldPerson.getSource());
+                preparedStatement.setString(12, oldPerson.getIndustry());
+
+                preparedStatement.executeUpdate();
+                logger.info("Person updated in the database.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public ObservableList<Person> getAllPersons() {
         ObservableList<Person> personList = FXCollections.observableArrayList();
         try (Connection connection = DBConnection.getConnection()) {
@@ -57,6 +85,72 @@ public class DBServices {
         }
 
         return personList;
+    }
+
+    public ObservableList<Person> searchPersons(String name, String netWorth, Integer age, String countryOrTerritory, String source, String industry) {
+        ObservableList<Person> searchResults = FXCollections.observableArrayList();
+        try (Connection connection = DBConnection.getConnection()) {
+            String searchPersonsSQL = "SELECT * FROM person WHERE "
+                    + "(? IS NULL OR name LIKE ?) AND "
+                    + "(? IS NULL OR netWorth LIKE ?) AND "
+                    + "(? IS NULL OR age = ?) AND "
+                    + "(? IS NULL OR countryOrTerritory LIKE ?) AND "
+                    + "(? IS NULL OR source LIKE ?) AND "
+                    + "(? IS NULL OR industry LIKE ?)";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(searchPersonsSQL)) {
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, name == null ? "%" : "%" + name + "%");
+                preparedStatement.setString(3, netWorth);
+                preparedStatement.setString(4, netWorth == null ? "%" : "%" + netWorth + "%");
+                preparedStatement.setObject(5, age);
+                preparedStatement.setObject(6, age);
+                preparedStatement.setString(7, countryOrTerritory);
+                preparedStatement.setString(8, countryOrTerritory == null ? "%" : "%" + countryOrTerritory + "%");
+                preparedStatement.setString(9, source);
+                preparedStatement.setString(10, source == null ? "%" : "%" + source + "%");
+                preparedStatement.setString(11, industry);
+                preparedStatement.setString(12, industry == null ? "%" : "%" + industry + "%");
+
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()) {
+                        String resultName = resultSet.getString("name");
+                        String resultNetWorth = resultSet.getString("netWorth");
+                        int resultAge = resultSet.getInt("age");
+                        String resultCountryOrTerritory = resultSet.getString("countryOrTerritory");
+                        String resultSource = resultSet.getString("source");
+                        String resultIndustry = resultSet.getString("industry");
+                        Person resultPerson = new Person(resultName, resultNetWorth, resultAge, resultCountryOrTerritory, resultSource, resultIndustry);
+                        searchResults.add(resultPerson);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return searchResults;
+    }
+
+
+    public void deletePerson(Person person) {
+        try (Connection connection = DBConnection.getConnection()) {
+            String deletePersonSQL = "DELETE FROM person WHERE name=? AND netWorth=? AND age=? AND countryOrTerritory=? AND source=? AND industry=?";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(deletePersonSQL)) {
+                preparedStatement.setString(1, person.getName());
+                preparedStatement.setString(2, person.getNetWorth());
+                preparedStatement.setInt(3, person.getAge());
+                preparedStatement.setString(4, person.getCountryOrTerritory());
+                preparedStatement.setString(5, person.getSource());
+                preparedStatement.setString(6, person.getIndustry());
+
+                preparedStatement.executeUpdate();
+                logger.info("Person deleted from the database.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/heshanthenura/teamrocket/MainController.java
+++ src/main/java/com/heshanthenura/teamrocket/MainController.java
@@ -9,11 +9,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 
@@ -29,6 +32,7 @@ public class MainController implements Initializable {
 
     @FXML
     private AnchorPane root;
+
 
     @FXML
     private TableView<Person> dataTable;
@@ -46,15 +50,58 @@ public class MainController implements Initializable {
     private TableColumn<Person, String> industryColumn;
 
 
+    @FXML
+    private TextField ageField;
+    @FXML
+    private TextField countryOrTerritoryField;
+    @FXML
+    private Button deleteBtn;
+    @FXML
+    private TextField industryField;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField netWorthField;
+    @FXML
+    private Button searchBtn;
+    @FXML
+    private TextField sourceField;
+    @FXML
+    private Button updateBtn;
+
     ObservableList<Person> personList = FXCollections.observableArrayList();
     DBServices dbServices = new DBServices();
     Logger logger = Logger.getLogger("info");
 
+    @FXML
+    void deleteSelectedPerson(MouseEvent event) {
+        Person selectedPerson = dataTable.getSelectionModel().getSelectedItem();
+        if (selectedPerson != null) {
+            dbServices.deletePerson(selectedPerson);
+            personList.remove(selectedPerson);
+            clearFormFields();
+        }
+    }
+
+
+    @FXML
+    void searchPersons(MouseEvent event) {
+            String name = nameField.getText();
+            String netWorth = netWorthField.getText();
+            Integer age = (ageField.getText().isEmpty()) ? null : Integer.parseInt(ageField.getText());
+            String countryOrTerritory = countryOrTerritoryField.getText();
+            String source = sourceField.getText();
+            String industry = industryField.getText();
+            ObservableList<Person> searchResults = dbServices.searchPersons(name, netWorth, age, countryOrTerritory, source, industry);
+            personList.setAll(searchResults);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Platform.runLater(()->{
 
+        Platform.runLater(()->{
             populateTable();
+            getSelectedPersonData();
             dataTable.setItems(personList);
             root.setOnDragOver(this::onDragOver);
             root.setOnDragDropped(e->{
@@ -64,7 +111,9 @@ public class MainController implements Initializable {
                     throw new RuntimeException(ex);
                 }
             });
+            updateBtn.setOnAction(event -> updateSelectedPerson());
         });
+
     }
 
     void populateTable(){
@@ -75,14 +124,46 @@ public class MainController implements Initializable {
             countryOrTerritoryColumn.setCellValueFactory(new PropertyValueFactory<>("countryOrTerritory"));
             sourceColumn.setCellValueFactory(new PropertyValueFactory<>("source"));
             industryColumn.setCellValueFactory(new PropertyValueFactory<>("industry"));
-
-//       personList.addAll(
-//                new Person("John Doe", "1000000", 30, "USA", "Tech Corp", "IT"),
-//                new Person("Jane Smith", "1500000", 35, "UK", "Finance Ltd", "Finance")
-//        );
             personList.addAll(dbServices.getAllPersons());
             dataTable.setItems(personList);
         });
+    }
+
+    void updateSelectedPerson() {
+        Person selectedPerson = dataTable.getSelectionModel().getSelectedItem();
+
+        if (selectedPerson != null) {
+            String updatedName = nameField.getText().isEmpty() ? selectedPerson.getName() : nameField.getText();
+            String updatedNetWorth = netWorthField.getText().isEmpty() ? selectedPerson.getNetWorth() : netWorthField.getText();
+            int updatedAge = ageField.getText().isEmpty() ? selectedPerson.getAge() : Integer.parseInt(ageField.getText());
+            String updatedCountryOrTerritory = countryOrTerritoryField.getText().isEmpty() ? selectedPerson.getCountryOrTerritory() : countryOrTerritoryField.getText();
+            String updatedSource = sourceField.getText().isEmpty() ? selectedPerson.getSource() : sourceField.getText();
+            String updatedIndustry = industryField.getText().isEmpty() ? selectedPerson.getIndustry() : industryField.getText();
+
+            // Create an updated Person object
+            Person updatedPerson = new Person(updatedName, updatedNetWorth, updatedAge, updatedCountryOrTerritory, updatedSource, updatedIndustry);
+
+            // Update the database
+            dbServices.updatePerson(selectedPerson, updatedPerson);
+
+            // Update the ObservableList
+            int selectedIndex = personList.indexOf(selectedPerson);
+            personList.set(selectedIndex, updatedPerson);
+
+            // Clear the form fields
+            clearFormFields();
+        }
+    }
+
+
+
+    void clearFormFields() {
+        nameField.clear();
+        netWorthField.clear();
+        ageField.clear();
+        countryOrTerritoryField.clear();
+        sourceField.clear();
+        industryField.clear();
     }
 
     void onDragOver(DragEvent event) {
@@ -108,6 +189,24 @@ public class MainController implements Initializable {
         }
         event.setDropCompleted(success);
         event.consume();
+    }
+
+    void getSelectedPersonData() {
+
+        dataTable.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 1) { // Check for a single click
+                Person selectedPerson = dataTable.getSelectionModel().getSelectedItem();
+                if (selectedPerson != null) {
+                    nameField.setText(selectedPerson.getName());
+                    netWorthField.setText(selectedPerson.getNetWorth());
+                    ageField.setText(String.valueOf(selectedPerson.getAge()));
+                    countryOrTerritoryField.setText(selectedPerson.getCountryOrTerritory());
+                    sourceField.setText(selectedPerson.getSource());
+                    industryField.setText(selectedPerson.getIndustry());
+                }
+            }
+        });
+
     }
 
 }
